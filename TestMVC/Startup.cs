78f32916211ce67f9fd3dd6a973993180a3bdf8d
Patrick Wong <patrickkei@gmail.com>
2021-11-04 18:28:32 +0800

    20211104

--- TestMVC/Startup.cs
+++ TestMVC/Startup.cs
@@ -27,6 +27,7 @@ namespace TestMVC
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                // 222
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
