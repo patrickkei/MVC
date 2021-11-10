3b2f1cfb6ab608c228cc1e6fcb255fc22247ac4f
Patrick Wong <patrickkei@gmail.com>
2021-11-10 15:09:28 +0800

    aaaaaaaa

--- TestMVC/Startup.cs
+++ TestMVC/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 //Master
-//0011
+//0022
 namespace TestMVC
 {
     public class Startup
